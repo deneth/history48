8d88b0518c46368b9c5fdeb70c8328ea88500efa
deneth <deneth@gmx.fr>
2018-03-20 16:43:36 +0100

    pdo

--- lib/connexion_pdo.php
+++ lib/connexion_pdo.php
@@ -3,15 +3,9 @@ try
 {
 	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 	
-	// pour MySQL
-	//$bdd = new PDO('mysql:host=localhost;port=3306;dbname=cdismo', 'cdismo', 'cdismo01', $pdo_options);
-	
-	// pour MariaDB chez CDISMO
-	//$bdd = new PDO('mysql:host=localhost;port=3307;dbname=akb48', 'cdismo', 'cdismo01', $pdo_options);
-	
-	// pour MariaDB sur kinsufi2
-	$bdd = new PDO('mysql:host=188.165.254.73;port=3306;dbname=akb48', 'deneth', 'corwin48', $pdo_options);
-	
+	// connexion avec les infos du fichier de config
+	$bdd = new PDO('mysql:host='.BDD_HOST.';port='.BDD_PORT.';dbname='.BDD_BDD.'', ''.BDD_USER.'', ''.BDD_PASS.'', $pdo_options);
+
 	$bdd->query("SET NAMES 'utf8'"); // pour faire afficher les caracteres utf8
 	$bdd->query("SET lc_time_names = 'fr_FR'"); // pour avoir les dates en francais
 	
