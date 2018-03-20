9b0fd2f6a591915994798380467378354a58cde6
deneth <deneth@gmx.fr>
2018-03-20 14:23:43 +0100

    fix colors logos

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
 		<div class="ske48-lg w90">SKE48</div> -
 		<div class="nmb48-lg w90">NMB48</div> -
 		<div class="hkt48-lg w90">HKT48</div> -
-		<div class="sdn48-lg w90">SDN48</div> -
-		<div class="ngt48-lg w90">NGT48</div> -
-		<div class="stu48-lg w90">STU48</div> -
+		<div class="sdn48-lg w90 sdn">SDN48</div> -
+		<div class="ngt48-lg w90 ngt">NGT48</div> -
+		<div class="stu48-lg w90 stu">STU48</div> -
 		<div class="nogizaka46-lg w90">NGZ46</div> -
-		<div class="kykz46-lg w90">KKZ46</div>
+		<div class="keyakizaka46-lg w90 sdn">KKZ46</div>
 	</h2>
 	<div>
 		<img src="images/AKB48_logo.svg" width=100 height=150 alt="AKB48 Logo" />
